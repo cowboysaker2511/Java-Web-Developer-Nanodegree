d348864295821dbbd6e3d5a867184d134fc91a3f
cowboysaker2511 <cowboysaker2511@gmail.com>
2023-07-09 23:26:33 +0700

    eureka deploy demo

---
+++ Projects/Demo/Microservice_Demo/src/main/java/Microservice_Demo/Microservice_Demo/MicroserviceDemoApplication.java
@@ -0,0 +1,15 @@
+package Microservice_Demo.Microservice_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroserviceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDemoApplication.class, args);
+	}
+
+}
---
+++ Projects/Demo/Microservice_Demo/src/test/java/Microservice_Demo/Microservice_Demo/MicroserviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Microservice_Demo.Microservice_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
